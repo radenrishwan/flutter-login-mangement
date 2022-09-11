9d4aab6bd1e7ae51b94d176236616b457455463d
radenrishwan <radenrishwan@gmail.com>
2022-09-11 07:04:24 +0700

    add internet permission

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,3 @@
 /// You can check github repository for demo api
 /// Example : localhost:3000/api/v1
-const String endpoint = 'endpointapihere';
+const String endpoint = 'https://seiornote-pq-2wepigvexa-et.a.run.app/api/v1';
